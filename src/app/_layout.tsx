4f688959e6b45de5b426a07ec48aaa50ef81e808
Anurag <anuragbhuyan240@gmail.com>
2025-07-10 22:21:16 +0530

    Setup TanStack Query and implement the first useQuery()

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,9 @@ import { Slot } from "expo-router";
 
 import { ThemeProvider, DarkTheme } from "@react-navigation/native";
 import { AuthProvider } from "@/providers/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const myTheme = {
   ...DarkTheme,
@@ -17,9 +20,11 @@ const myTheme = {
 export default function RootLayout() {
   return (
     <ThemeProvider value={myTheme}>
-      <AuthProvider>
-        <Slot />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Slot />
+        </AuthProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
